15a3c67287246d24f63b4e8636d89e948071671e
Nghia Le <dansharisan@gmail.com>
2019-10-10 18:33:15 +0900

    Make coreui worked

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,20 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
+
+mix.webpackConfig({
+    devServer: { disableHostCheck: true },
+    resolve  : {
+        alias: {
+            '@'         : path.resolve(__dirname, 'resources/js/'),
+            'static'    : path.resolve(__dirname, 'resources/static/')
+        },
+    }
+})
+
+if (mix.inProduction())
+    mix.version()
+else
+    mix.sourceMaps()
