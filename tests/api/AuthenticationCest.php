d9864715a33750cdf220d0b348b193274faca775
Nghia Le <nghia.le@careercross.com>
2020-03-06 13:46:12 +0900

    Skeleton for Codecept on Local

--- tests/api/AuthenticationCest.php
+++ tests/api/AuthenticationCest.php
@@ -1,9 +1,16 @@
 <?php
+use Symfony\Component\HttpFoundation\Response as Response;
 
 class AuthenticationCest
 {
-    public function register(ApiTester $I)
+    public function loginFailure(ApiTester $I)
     {
-        
+        // Send the request
+        $I->sendPOST('/auth/login', [
+            'email' => 'fake_email@something.com',
+            'password' => 'anything'
+        ]);
+
+        $I->seeResponseCodeIs(Response::HTTP_UNAUTHORIZED);
     }
 }
