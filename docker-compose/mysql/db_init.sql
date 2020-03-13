fd2cd58ec0b66f58d5da5b1aa4dffd733f0ab59d
Nghia Le <dansharisan@gmail.com>
2020-03-13 09:17:25 +0900

    Slightly change on MySQL config

--- docker-compose/mysql/db_init.sql
+++ docker-compose/mysql/db_init.sql
@@ -1,5 +1,6 @@
-CREATE TABLE IF NOT EXISTS `stenup`;
-DROP TABLE IF EXISTS `stenup_test`;
+CREATE DATABASE IF NOT EXISTS `stenup`;
+DROP DATABASE IF EXISTS `stenup_test`;
 CREATE DATABASE `stenup_test`;
-CREATE USER 'tester'@'db' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON stenup_test . * TO 'tester'@'db';
+CREATE USER 'tester'@'%' IDENTIFIED BY 'p@ssw0rd';
+GRANT ALL PRIVILEGES ON stenup_test . * TO 'tester'@'%';
+FLUSH PRIVILEGES;
