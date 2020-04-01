b5570b5e3b7f0a03c82ef32864477a548307aaf3
Nghia Le <nghia.le@careercross.com>
2020-04-01 17:57:36 +0900

    Start implementing test for /api/users api

--- tests/api/UserCest.php
+++ tests/api/UserCest.php
@@ -8,9 +8,23 @@ class UserCest
     use UtilTrait;
     /**
     * Endpoint: GET /api/users
+    * Depends on: login
     **/
     public function index(ApiTester $I)
     {
+        // Prepare data
+        $memberUser = $I->generateMemberUser();
 
+        /* Case: Calling the API while not logged in should return unauthorized error */
+        $I->sendGET('/api/users');
+        $I->seeUnauthorizedRequestError();
+
+        /* Case: By default, member user, which normally don't have VIEW_USERS permission, shouldn't be able to access this API */
+        $I->sendPOST('/api/auth/login', [
+            'email' => $memberUser->email,
+            'password' => 'password'
+        ]);
+        $I->sendGET('/api/users');
+        $I->seeUnauthorizedRequestError();
     }
 }
